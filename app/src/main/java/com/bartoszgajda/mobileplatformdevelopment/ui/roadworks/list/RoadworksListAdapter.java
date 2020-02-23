1dc35d8de7fdc45d8daf4e71f543dcb5adf2a713
Bartosz Gajda <bg@bartoszgajda.com>
2020-02-23 17:28:25 +0000

    Add roadwork length in days.

--- app/src/main/java/com/bartoszgajda/mobileplatformdevelopment/ui/roadworks/list/RoadworksListAdapter.java
+++ app/src/main/java/com/bartoszgajda/mobileplatformdevelopment/ui/roadworks/list/RoadworksListAdapter.java
@@ -3,6 +3,7 @@ package com.bartoszgajda.mobileplatformdevelopment.ui.roadworks.list;
 import android.content.Context;
 import android.graphics.Color;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,14 @@ import android.widget.TextView;
 import com.bartoszgajda.mobileplatformdevelopment.R;
 import com.bartoszgajda.mobileplatformdevelopment.util.model.RoadworkModel;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class RoadworksListAdapter extends ArrayAdapter<RoadworkModel> {
   public RoadworksListAdapter(Context context, List<RoadworkModel> roadworks) {
@@ -28,6 +35,7 @@ public class RoadworksListAdapter extends ArrayAdapter<RoadworkModel> {
     }
     TextView roadworkTitle = (TextView) convertView.findViewById(R.id.roadwork_title);
     TextView roadworkType = (TextView) convertView.findViewById(R.id.roadwork_type);
+    TextView roadworkLength = (TextView) convertView.findViewById(R.id.roadwork_length);
     TextView roadworkDescription = (TextView) convertView.findViewById(R.id.roadwork_description);
     TextView roadworkLink = (TextView) convertView.findViewById(R.id.roadwork_link);
     TextView roadworkPubdate = (TextView) convertView.findViewById(R.id.roadwork_pubdate);
@@ -43,6 +51,22 @@ public class RoadworksListAdapter extends ArrayAdapter<RoadworkModel> {
     } else {
       roadworkType.setText(R.string.roadworks_type_planned);
       roadworkType.setTextColor(parent.getResources().getColor(R.color.brandYellow));
+    }
+
+    String[] splitDescription = roadwork.getDescription().split("\\s+");
+    final Calendar startCalendar = Calendar.getInstance();
+    final Calendar endCalendar = Calendar.getInstance();
+
+    try {
+      Date startDate = new SimpleDateFormat("dd/MMMM/yyyy").parse(splitDescription[3] + "/" + splitDescription[4] + "/" + splitDescription[5]);
+      startCalendar.setTime(startDate);
+      Date endDate = new SimpleDateFormat("dd/MMMM/yyyy").parse(splitDescription[11] + "/" + splitDescription[12] + "/" + splitDescription[13]);
+      endCalendar.setTime(endDate);
+
+      Long lengthInDays = TimeUnit.MILLISECONDS.toDays(Math.abs(startCalendar.getTimeInMillis() - endCalendar.getTimeInMillis()));
+      roadworkLength.setText("Roadwork Length in Days: " + lengthInDays);
+    } catch (ParseException e) {
+      Log.e("roadwork", e.getMessage());
     }
 
     return convertView;
