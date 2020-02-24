e13772f27d97eb57657c28b9b0c97f04de54edab
Bartosz Gajda <bg@bartoszgajda.com>
2020-02-24 20:45:38 +0000

    Add onClickListener to plan button.

--- app/src/main/java/com/bartoszgajda/mobileplatformdevelopment/ui/planner/PlannerFragment.java
+++ app/src/main/java/com/bartoszgajda/mobileplatformdevelopment/ui/planner/PlannerFragment.java
@@ -1,9 +1,12 @@
 package com.bartoszgajda.mobileplatformdevelopment.ui.planner;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,9 +19,12 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
-public class PlannerFragment extends Fragment implements OnMapReadyCallback {
+public class PlannerFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
 
   private PlannerViewModel plannerViewModel;
+  private EditText origin;
+  private EditText destination;
+  private Button planButton;
   private MapView mapView;
   private GoogleMap googleMap;
 
@@ -26,11 +32,23 @@ public class PlannerFragment extends Fragment implements OnMapReadyCallback {
     plannerViewModel = ViewModelProviders.of(this).get(PlannerViewModel.class);
     View root = inflater.inflate(R.layout.fragment_planner, container, false);
 
+    this.origin = (EditText) root.findViewById(R.id.planner_origin);
+    this.destination = (EditText) root.findViewById(R.id.planner_destination) ;
+    this.planButton = (Button) root.findViewById(R.id.planner_plan);
     this.mapView = (MapView) root.findViewById(R.id.planner_map);
+
     this.mapView.onCreate(savedInstanceState);
     this.mapView.getMapAsync(this);
 
+    this.planButton.setOnClickListener(this);
+
     return root;
+  }
+
+  @Override
+  public void onClick(View view) {
+    Log.d("planner", "Origin: " + this.origin.getText());
+    Log.d("planner", "Destination: " + this.destination.getText());
   }
 
   @Override
