b38f03cc3e92aef8a7274d3a11bae72134d41384
Bartosz Gajda <bg@bartoszgajda.com>
2020-02-25 19:37:20 +0000

    Add drawing polyline on map between two cities.

--- app/src/main/java/com/bartoszgajda/mobileplatformdevelopment/ui/planner/PlannerFragment.java
+++ app/src/main/java/com/bartoszgajda/mobileplatformdevelopment/ui/planner/PlannerFragment.java
@@ -1,5 +1,6 @@
 package com.bartoszgajda.mobileplatformdevelopment.ui.planner;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +19,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlannerFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
 
@@ -33,7 +47,7 @@ public class PlannerFragment extends Fragment implements OnMapReadyCallback, Vie
     View root = inflater.inflate(R.layout.fragment_planner, container, false);
 
     this.origin = (EditText) root.findViewById(R.id.planner_origin);
-    this.destination = (EditText) root.findViewById(R.id.planner_destination) ;
+    this.destination = (EditText) root.findViewById(R.id.planner_destination);
     this.planButton = (Button) root.findViewById(R.id.planner_plan);
     this.mapView = (MapView) root.findViewById(R.id.planner_map);
 
@@ -54,7 +68,65 @@ public class PlannerFragment extends Fragment implements OnMapReadyCallback, Vie
   @Override
   public void onMapReady(GoogleMap googleMap) {
     this.googleMap = googleMap;
+
     LatLng glasgow = new LatLng(55.86515, -4.25763);
+    this.googleMap.addMarker(new MarkerOptions().position(glasgow));
+    LatLng edinburgh = new LatLng(55.953251, -3.188267);
+    this.googleMap.addMarker(new MarkerOptions().position(edinburgh));
+
+    List<LatLng> path = new ArrayList<>();
+    GeoApiContext context = new GeoApiContext.Builder().apiKey(getResources().getString(R.string.google_maps_key)).build();
+    DirectionsApiRequest req = DirectionsApi.getDirections(context, "55.86515,-4.25763", "55.953251,-3.188267");
+    try {
+      DirectionsResult res = req.await();
+
+      //Loop through legs and steps to get encoded polylines of each step
+      if (res.routes != null && res.routes.length > 0) {
+        DirectionsRoute route = res.routes[0];
+
+        if (route.legs !=null) {
+          for(int i=0; i<route.legs.length; i++) {
+            DirectionsLeg leg = route.legs[i];
+            if (leg.steps != null) {
+              for (int j=0; j<leg.steps.length;j++){
+                DirectionsStep step = leg.steps[j];
+                if (step.steps != null && step.steps.length >0) {
+                  for (int k=0; k<step.steps.length;k++){
+                    DirectionsStep step1 = step.steps[k];
+                    EncodedPolyline points1 = step1.polyline;
+                    if (points1 != null) {
+                      //Decode polyline and add points to list of route coordinates
+                      List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                      for (com.google.maps.model.LatLng coord1 : coords1) {
+                        path.add(new LatLng(coord1.lat, coord1.lng));
+                      }
+                    }
+                  }
+                } else {
+                  EncodedPolyline points = step.polyline;
+                  if (points != null) {
+                    //Decode polyline and add points to list of route coordinates
+                    List<com.google.maps.model.LatLng> coords = points.decodePath();
+                    for (com.google.maps.model.LatLng coord : coords) {
+                      path.add(new LatLng(coord.lat, coord.lng));
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    } catch (Exception e) {
+      Log.e("planner", e.getLocalizedMessage());
+    }
+
+    //Draw the polyline
+    if (path.size() > 0) {
+      PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLACK).width(5);
+      this.googleMap.addPolyline(opts);
+    }
+
     this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(glasgow, 6));
     this.googleMap.getUiSettings().setZoomControlsEnabled(true);
   }
